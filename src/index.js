dd1af41b4a96a1dd116e6e39736b850760469da6
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-18 15:55:32 -0300

    :rocket: Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Background from './components/Background';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <Background>
+      <Text>Hello app</Text>
+    </Background>
+  );
+}
