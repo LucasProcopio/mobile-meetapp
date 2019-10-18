42af0cbe3c2af69eb7cb50429910e80f2896d5f8
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-18 16:58:19 -0300

    :pencil: chore: add eslint config file

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,8 +1,8 @@
-import LinearGradient from 'react-native-linear-gradient';
-import styled from 'styled-components/native';
+import LinearGradient from "react-native-linear-gradient";
+import styled from "styled-components/native";
 
 export default styled(LinearGradient).attrs({
-  colors: ['#22202C', '#402845'],
+  colors: ["#22202C", "#402845"],
 })`
   flex: 1;
 `;
